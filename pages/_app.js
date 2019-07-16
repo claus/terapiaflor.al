35ae3c482563bf3c2007e43c3e076e022e35b18e
Claus Wahlers <claus@codeazur.com.br>
2019-07-16 20:44:26 -0300

    Add favicons

--- pages/_app.js
+++ pages/_app.js
@@ -3,27 +3,19 @@ import App, { Container } from 'next/app';
 import Head from 'next/head';
 
 class TerapiaFloralApp extends App {
-    // static async getInitialProps({ Component, ctx }) {
-    //     return {
-    //         pageProps: {
-    //             ...(Component.getInitialProps
-    //                 ? await Component.getInitialProps(ctx)
-    //                 : {}),
-    //         },
-    //     };
-    // }
-
     renderHead() {
         return (
             // prettier-ignore
             <Head>
                 <meta charSet="utf-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
-                <meta key="meta-og-url" property="og:url" content="https://terapiaflor.al/" />
-                <meta key="meta-og-type" property="og:type" content="website" />
-                <title key="title">Terapia Floral</title>
-                <link rel="shortcut icon" href="/static/favicon.ico" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <title>Daniela Wahlers — Terapeuta Floral</title>
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://terapiaflor.al/" />
+                <link rel="icon" href="/static/favicon-32.png" sizes="32x32" />
+                <link rel="icon" href="/static/favicon-64.png" sizes="64x64" />
+                <link rel="icon" href="/static/favicon-128.png" sizes="128x128" />
             </Head>
         );
     }

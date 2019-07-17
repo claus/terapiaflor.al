d75fca7623dca0ebd38df9d268397caaf21eff9b
Claus Wahlers <claus@codeazur.com.br>
2019-07-17 04:56:21 -0300

    Fetch landing from Contentful

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { connect } from 'react-redux';
+import { fetchLanding } from 'store';
 
 import Header from 'components/header';
 
 import styles from './index.scss';
 
-export default () => (
+const Landing = ({ landing }) => (
     <main className={styles.main}>
         <Header />
         <section>
             <Link href="/about">
                 <a className={styles.link}>Go to About Me</a>
             </Link>
+            <pre>{JSON.stringify(landing, null, 2)}</pre>
         </section>
     </main>
 );
+
+Landing.getInitialProps = ({ store }) => {
+    return store.dispatch(fetchLanding()).then(() => {
+        const state = store.getState();
+        return {
+            landing: state.landing,
+        };
+    });
+};
+
+Landing.propTypes = {
+    landing: PropTypes.object.isRequired,
+    fetchLanding: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+    landing: state.landing,
+});
+
+export default connect(
+    mapStateToProps,
+    { fetchLanding }
+)(Landing);

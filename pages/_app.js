bd8459fb40309672efcdb283b813c236d402e806
Claus Wahlers <claus@codeazur.com.br>
2019-07-18 18:12:34 -0300

    Add some meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -12,8 +12,14 @@ class TerapiaFloralApp extends App {
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <title>Daniela Wahlers — Terapeuta Floral</title>
+                <meta name="robots" content="index, follow" />
+                <meta name="description" content="Encontre cura e paz, sem sair de casa." />
+                <meta property="og:locale" content="pt-BR" />
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://terapiaflor.al/" />
+                <meta property="og:title" content="Daniela Wahlers — Terapeuta Floral" />
+                <meta property="og:description" content="Encontre cura e paz, sem sair de casa." />
+                <meta property="og:url" content="https://terapiaflor.al" />
+                <link rel="canonical" href="http://terapiaflor.al" />
                 <link rel="icon" href="/static/favicon-32.png" sizes="32x32" />
                 <link rel="icon" href="/static/favicon-64.png" sizes="64x64" />
                 <link rel="icon" href="/static/favicon-128.png" sizes="128x128" />

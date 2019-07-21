9305f5144b0b67fa83e5c996f00862e401351df7
Claus Wahlers <claus@codeazur.com.br>
2019-07-21 07:12:11 -0300

    Add logo

--- components/ui/Header.js
+++ components/ui/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import styles from './Header.scss';
+
 const Header = () => (
     <header>
-        <h1>Daniela Wahlers — Terapeuta Floral</h1>
+        <h1>
+            <img className={styles.logo} src="/static/logo.svg" />
+        </h1>
     </header>
 );
 

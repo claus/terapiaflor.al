1aced6ddab8a7869086997fc1876479d51f9d26f
Claus Wahlers <claus@codeazur.com.br>
2019-12-15 18:22:01 -0300

    Fix prop types

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import styles from './Footer.scss';
 
@@ -34,9 +33,5 @@ const Footer = () => (
         </div>
     </footer>
 );
-
-Footer.propTypes = {
-    subtitle: PropTypes.string.isRequired,
-};
 
 export default Footer;
